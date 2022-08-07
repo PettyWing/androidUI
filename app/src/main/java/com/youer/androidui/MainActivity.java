05582a337659a5019fc62a44e33378e605815c42
悠二 <youer.xyc@alibaba-inc.com>
2022-08-07 19:40:12 +0800

    feature：迁移为Androidx

--- app/src/main/java/com/youer/androidui/MainActivity.java
+++ app/src/main/java/com/youer/androidui/MainActivity.java
@@ -3,7 +3,7 @@ package com.youer.androidui;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
